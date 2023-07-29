f683550cab3aadccbcf6374c965ba171dd34208a
Anas-Wael <anaswael600@gmail.com>
2023-07-30 01:20:16 +0200

    Added the heap files

---
+++ app/Data_structures/heap.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace app\Data_structures;
+
+class MaxHeap
+{
+    public $data = [];
+
+    public function BuildHeap(array $values)
+    {
+        foreach($values as $value)
+        {
+            $this->heapPush($value);
+        }
+    }
+
+    public function heapPush($value)
+    {
+        // Add the value to the end of the heap
+        $this->data[] = $value;
+
+        $this->heapify();
+    }
+
+    public function heapify()
+    {
+        // Start at the last element in the heap and then swap every child with its parent if there is a violation of the heap property
+        for ($i = count($this->data) - 1; $i > 0; $i--)
+        {
+            $parent_index = intdiv($i - 1, 2);
+            if ($this->data[$i] > $this->data[$parent_index])
+            {
+                // Swap
+                $temp = $this->data[$i];
+                $this->data[$i] = $this->data[$parent_index];
+                $this->data[$parent_index] = $temp;
+            }
+        }
+    }
+
+    public function get_peek()
+    {
+        return $this->data[0];
+    }
+
+    public function remove($value)
+    {
+        $index = array_search($value, $this->data);
+        // Swap the value with the farthest right value which in our case the last element in the array
+        $this->data[$index] = $this->data[count($this->data) - 1];
+        array_pop($this->data);
+
+        // Restore the heap property
+        $this->heapify();
+    }
+
+    public function heapSort()
+    {
+        $sorted_array = [];
+        while (count($this->data) !== 0)
+        {
+            $sorted_array[] = $this->data[0];
+            array_shift($this->data);
+            $this->heapify();
+        }
+        return $sorted_array;
+    }
+}
+
+class MinHeap
+{
+    public $data = [];
+
+    public function BuildHeap(array $values)
+    {
+        foreach($values as $value)
+        {
+            $this->heapPush($value);
+        }
+    }
+
+    public function heapPush($value)
+    {
+       $this->data[] = $value;
+
+      $this->heapify();
+    }
+
+    public function get_peek()
+    {
+        return $this->data[0];
+    }
+
+    public function heapify()
+    {
+        for ($i = count($this->data) - 1; $i > 0; $i--)
+        {
+            $parent_index = intdiv($i - 1, 2);
+            if ($this->data[$i] < $this->data[$parent_index])
+            {
+                // Swap
+                $temp = $this->data[$i];
+                $this->data[$i] = $this->data[$parent_index];
+                $this->data[$parent_index] = $temp;
+            }
+        }
+    }
+
+    public function heapSort()
+    {
+        $sorted_array = [];
+        while (count($this->data) !== 0)
+        {
+            $sorted_array[] = $this->data[0];
+            array_shift($this->data);
+            $this->heapify();
+        }
+        return $sorted_array;
+    }
+}
---
+++ tests/unit/Data_structures/heapTest.php
@@ -0,0 +1,130 @@
+<?php
+
+require_once "vendor/autoload.php";
+require_once "app/Data_structures/heap.php";
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProviderExternal;
+
+final class heapTest extends TestCase
+{
+    public $MaxHeap;
+    public $MinHeap;
+    public function setUp():void
+    {
+        $this->MaxHeap = new app\Data_structures\MaxHeap;
+        $this->MinHeap = new app\Data_structures\MinHeap;
+    }
+
+    #[DataProviderExternal('heapTestDataProviders', 'test_heapifying_an_array_into_a_max_heap_data_provider')]
+    public function test_heapifying_an_array_into_a_max_heap($values, $expected_heap)
+    {
+        $this->MaxHeap->BuildHeap($values);
+        $this->assertEquals($expected_heap, $this->MaxHeap->data);
+    }
+
+    #[DataProviderExternal('heapTestDataProviders', 'test_heapifying_an_array_into_a_min_heap_data_provider')]
+    public function test_heapifying_an_array_into_a_min_heap($values, $expected_heap)
+    {
+        $this->MinHeap->BuildHeap($values);
+        $this->assertEquals($expected_heap, $this->MinHeap->data);
+    }
+
+    #[DataProviderExternal('heapTestDataProviders', 'test_peeking_the_heap_data_provider')]
+    public function test_peeking_the_heap($values, $peek)
+    {
+        $this->MinHeap->BuildHeap($values);
+        $this->assertEquals($this->MinHeap->get_peek(), $this->MinHeap->data[0]);
+    }
+
+    #[DataProviderExternal('heapTestDataProviders', 'test_removing_a_value_from_the_heap_data_provider')]
+    public function test_removing_a_value_from_the_heap($values, $value, $expected_heap)
+    {
+        $this->MaxHeap->BuildHeap($values);
+        $this->MaxHeap->remove($value);
+        $this->assertEquals($expected_heap ,$this->MaxHeap->data);
+    }
+
+    #[DataProviderExternal('heapTestDataProviders', 'test_heap_sorting_data_provider')]
+    public function test_heap_sorting($values, $expected_MaxHeap, $expected_MinHeap)
+    {
+        $this->MaxHeap->BuildHeap($values);
+        $this->MinHeap->BuildHeap($values);
+        $this->assertEquals($expected_MaxHeap ,$this->MaxHeap->heapSort());
+        $this->assertEquals($expected_MinHeap ,$this->MinHeap->heapSort());
+    }
+}
+
+class heapTestDataProviders
+{
+    public static function test_heapifying_an_array_into_a_max_heap_data_provider()
+    {
+        return [
+            // Order of input:  1:array of values  2:expected_heap
+            [[1], [1]],
+
+            [[1, 2, 3, 4, 5, 6], [6, 4, 5, 1, 3, 2]],
+
+            [[5, 7, 1, 4, 8, 6], [8, 7, 6, 4, 5, 1]],
+
+            [[5, 4, 3, 2, 1], [5, 4, 3, 2, 1]],
+
+        ];
+    }
+
+    public static function test_heapifying_an_array_into_a_min_heap_data_provider()
+    {
+        return [
+            // Order of input:  1:array of values  2:expected_heap
+            [[1], [1]],
+
+            [[5, 7, 1, 4, 8, 6], [1, 4, 5, 7, 8, 6]],
+            
+            [[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]],
+        ];
+    }
+
+    public static function test_peeking_the_heap_data_provider()
+    {
+        return [
+            // Order of input:  1:array of values  2:peek
+            [[1], 1],
+
+            [[5, 7, 1, 4, 8, 6], 1],
+            
+            [[1, 2, 3, 4, 5, 6], 1],
+        ];
+    }
+
+    public static function test_removing_a_value_from_the_heap_data_provider()
+    {
+        return [
+            // Order of input:  1:array of values  2:value to be removed  3:expected_heap
+
+            // Does not target anything just adds one test to our tests
+            [[1], 1, []],
+
+            // Target a peek value removal
+            [[1, 2, 3, 4, 5, 6], 6, [5, 4, 2, 1, 3]],
+
+            // Target a two children value removal
+            [[1, 2, 3, 4, 5, 6], 4, [6, 3, 5, 1, 2]],
+
+            // Target a one child value removal
+            [[1, 2, 3, 4, 5, 6], 5, [6, 4, 2, 1, 3]],
+
+            // Target a leaf value removal
+            [[1, 2, 3, 4, 5, 6], 2, [6, 4, 5, 1, 3]],
+
+        ];        
+    }
+
+    public static function test_heap_sorting_data_provider()
+    {
+        return [
+            // Order of input   1:Unsorted values    2: expected max heap     3: expected min heap
+            [[5, 7, 1, 4, 8, 6], [8, 7, 6, 5, 4, 1], [1, 4, 5, 6, 7, 8]],
+            [[3, 9, 6, 4, 12, 18], [18, 12, 9, 6, 4, 3], [3, 4, 6, 9, 12, 18]]
+        ];
+    }
+}
